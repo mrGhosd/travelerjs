312f91ace094a849f00672cc4f9a97fbc44f5063
vsokoltsov <vforvad@gmail.com>
2015-10-17 15:58:01 +0300

    add tour updating

--- app.js
+++ app.js
@@ -2,29 +2,43 @@ var app = angular.module('travelerjs', []);
 
 app.controller('IndexController', function($scope){
     $scope.title = "Список туров";
+    $scope.tourAction = '';
     $scope.showForm = false;
+    $scope.tourIndex = null;
     $scope.tours = localStorage['tours'] ? JSON.parse(localStorage['tours']) : [];
     $scope.toggleTourForm = function(){
+        $scope.tourAction = 'create';
+        $scope.tour = null;
         $scope.showForm = !$scope.showForm;
     };
-    console.log(localStorage);
     $scope.saveTour = function(){
-        tourForm = $scope.tour;
-        tour = {
+        var tourForm = $scope.tour;
+        var tour = {
                 title: tourForm.title,
                 text: tourForm.text,
                 price: tourForm.price,
-                contry: tourForm.country
+                country: tourForm.country
         };
-        $scope.tours.push(tour);
-        $scope.tour = null;
+        $scope.tourAction === 'create' ? $scope.tours.push(tour) : $scope.tours.splice($scope.tourIndex, 1, tour);
         $scope.showForm = false;
+        $scope.tourIndex = null;
+        saveToLocalStorage();
+    };
+
+    $scope.editTour = function($index){
+        $scope.tourIndex = $index;
+        $scope.tourAction = 'edit';
+        $scope.tour= $scope.tours[$index];
+        $scope.showForm = true;
+    };
+
+
+    function saveToLocalStorage(){
         localStorage['tours'] = JSON.stringify($scope.tours, function (key, val) {
             if (key == '$$hashKey') {
                 return undefined;
             }
             return val;
         });
-        console.log($scope.tours);
     }
 });

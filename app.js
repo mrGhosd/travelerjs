6c100969c9f36f94b6304dd6d58b5e9c36639680
vsokoltsov <vforvad@gmail.com>
2015-10-17 15:41:21 +0300

    add localstorage

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = angular.module('travelerjs', []);
 app.controller('IndexController', function($scope){
     $scope.title = "Список туров";
     $scope.showForm = false;
-    $scope.tours = [];
+    $scope.tours = localStorage['tours'] ? JSON.parse(localStorage['tours']) : [];
     $scope.toggleTourForm = function(){
         $scope.showForm = !$scope.showForm;
     };
-
+    console.log(localStorage);
     $scope.saveTour = function(){
         tourForm = $scope.tour;
         tour = {
@@ -19,6 +19,12 @@ app.controller('IndexController', function($scope){
         $scope.tours.push(tour);
         $scope.tour = null;
         $scope.showForm = false;
+        localStorage['tours'] = JSON.stringify($scope.tours, function (key, val) {
+            if (key == '$$hashKey') {
+                return undefined;
+            }
+            return val;
+        });
         console.log($scope.tours);
     }
 });

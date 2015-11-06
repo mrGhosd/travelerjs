cff41565b6f8b27fa8043a391239f337421d0f60
vsokoltsov <vforvad@gmail.com>
2015-11-06 15:42:34 +0300

    Add filtering by tour place

--- TravelsController.js
+++ TravelsController.js
@@ -2,10 +2,17 @@ angular.module('travelerjs').controller('TravelsController', ['tours', '$scope',
     function(tours, $scope, $http, places){
     $scope.title = "Список туров";
     $scope.tours = tours;
+    var toursList = tours;
     $scope.places = places;
-    console.log($scope.tours);
 
-    $scope.filterTours = function(){
+    $scope.filterTours = function(value){
+        var filterTours = [];
         var tours = angular.copy($scope.tours);
+        angular.forEach(toursList, function(value, index){
+           if(value.place.name === $scope.tour.place ){
+               filterTours.push(value);
+           }
+        });
+        $scope.tours = filterTours;
     }
 }]);
